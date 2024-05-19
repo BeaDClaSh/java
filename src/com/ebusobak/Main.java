123c915760ed5ff28d759f47f31cb520ed52aacf
AHUENEN2008 <ss576285@gmail.com>
2024-05-19 14:18:14 +0200

    java learning

---
+++ src/com/ebusobak/Main.java
@@ -0,0 +1,298 @@
+package com.ebusobak;
+
+public class Main {
+}
+/*import java.util.Scanner;
+public class main {
+
+    public static void main(String[] args) {// Comment
+        /*System.out.println("da zdrastvuet sanktpeterburg");
+
+
+
+        int age = 52;
+        // age= 52
+        System.out.println(age);
+
+        int pidor = 69;
+        System.out.println("skolko pidorov: " + pidor + " zelensky pidor");
+        pidor=420;
+        System.out.println(pidor);
+
+        byte gay =127;
+
+        /*byte -128 do 127 1 byte
+        short -32768 do 32767 2 byte
+        int -2147483648 do 2147483647 4 byte
+        long -9 223 372 036 854 775 808 do 9 223 372 036 854 775 807 8 byte
+         */ /* float num4=4.45676543f;
+         double num5=4.5676567d;
+         char ch='%';
+         String username="ebu telku";
+        System.out.println(username);
+        boolean isAutorDickLong = true; */
+// System.out.print("na skolko vy pidor:");
+//  Scanner lgbt =new Scanner(System.in);
+//String naskolkoPidor = lgbt.nextLine();
+//System.out.println("ty pidor na: " + naskolkoPidor);
+//int num8 = lgbt.nextInt();
+//byte num9 = lgbt.nextByte();
+ /* short num1 = 52, num2=69;
+  int huj=num1 * num2;
+        System.out.println("resultat:"+ huj)
+        % reszta
+.equals razreszaet programe chitat a ne tolko perepisywat dannye
+        ;*/
+        /* float num1 = 52, num2=69;
+  float huj=num1 % num2; huj+=10;
+        System.out.println("resultat:"+ huj);
+        */
+        /*System.out.println("vvedi 1 chyslo");
+        float num1 =lgbt.nextFloat();
+        System.out.println("vvedi 2 chyslo");
+        float num2=lgbt.nextFloat();
+        float res1=num1 +num2;
+                float res2=num1-num2;
+                        float res3=num1*num2;
+                                float res4=num1/num2;
+                                float res5=num1%num2;
+ravno eto == a ne =
+!= ne ravno
+prosto peremennaya =true
+! v nachale false
+else if tolko pered else
+|| eto ili
+&& nado 2 uslovia
+        System.out.println("result: "+ res1 + "\n" +res2 + "\n"+ res3+ "\n"+ res4+ "\n"+ res5);*/
+
+/*
+        int a=15, b=16;
+        char sym1='A',sym2='A';
+        boolean isHasDildo=true;
+                if (isHasDildo || a==b || sym1 !=sym2) {
+                    System.out.println("dildo na meste");
+                } else if (a>=b)
+        {
+            System.out.println("putin krasava");
+        } else {
+                    System.out.println("dilda netu");
+                }
+    }
+/*pidor of the night boolean true false */
+
+
+//import java.util.Scanner;
+
+//}
+//public class main {
+//  public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+        /*System.out.println("vvedite zvanie pidora:");
+        String role = scanner.nextLine();
+        System.out.println("vvedite parol pidora:");
+        String pass = scanner.nextLine()
+                ;
+     if (role.equals("gayskiy admin") && pass.equals("chornyj pidor"))
+    System.out.println("vse pidorki");
+            else {
+                System.out.println("privet pidaras! chem tebia uiebat?");
+                        String geystwo = scanner.nextLine();
+                System.out.print("privet chuilush po imeni "+ geystwo);*/
+//   System.out.print("vvedi chislo gomika 1:");
+// int num1 = scanner.nextInt();
+       /* switch (num) {
+            case 1:
+                System.out.println("pidorok vsego odin");
+                System.out.println("pidorok odinok");
+                break;
+                case 2:
+                    System.out.println("para golubkov pidorkov");
+                    break;
+            default:
+                System.out.println("pidorov netu"); */
+
+     /*   System.out.print("vvedi chislo gomika 1:");
+        int num1 = scanner.nextInt();
+
+        System.out.print("vvedi chislo gomika 2:");
+        int num2 = scanner.nextInt();
+
+        System.out.print("vvedi deistvie gomika :");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+int res;
+
+        switch (action) {
+            case "+":
+                res=num1+num2;
+                System.out.println("pidorskij itog "+ res );
+                break;
+            case "-":
+                res=num1-num2;
+                System.out.println("itog gejskij "+ res);
+                break;
+            case "*":
+                res=num1*num2;
+                System.out.println("itog gejskij " + res);
+                break;
+            case "/":
+                if(num2==0)
+                    System.out.println("idi nahuj na noll ne delitsa");
+                else {res=num1/num2;
+                System.out.println("pidorkij itog " + res);}
+                break;
+            case "%":
+                res=num1%num2;
+                System.out.println("pidorskij itog "+ res);
+                break;
+            default:
+
+                System.out.println("вы истинный пидор");
+cycles
+*/
+//        for (float i = 100; i > 1; i/=2) {
+//          System.out.println("geyskay peremennaya:" + i);
+//    }
+//}
+//}
+        /*int i =100;
+while(i>=5) {
+    System.out.println(i + " eto gejskaja peremennaya");
+i-=25;
+}*/
+//int i=100;
+//do{
+//  System.out.println(i+"do wsegda wywodit perviy raz a potom tolko proweriaet");
+//i*=100;
+//} while (i<10);
+//for (int i=5;i<69;i+=3){
+//  if (i%3==0)
+// skipaet cycl kotoryj sootvetstwuet usloviju   continue;
+//if (i>=52)
+//  break;
+//System.out.println("natural ebanyj"+i);
+//new vydaet pamiat
+//masivy dannych
+//int[] nums = new int[5];
+//nums[0] = 52;
+//nums[1] = 2137;
+//nums[2] = 228;
+//nums[3] = 69;
+//nums[4] = 420;
+//int res = nums[1] - nums[3];
+//System.out.println(nums[0]);
+//System.out.println(res);
+
+//float[] gay = new float[]{78f, 46f, 69.69f};
+//System.out.println(gay[2]);
+
+//for (int i = 0; i < gay.length; i++) {
+//   System.out.println("pidorent:"+gay[i]);
+//  }
+
+// public class main {
+//   public static void main(String[] args) {
+
+           /* int[] arr = new int[4];
+            Scanner scannerPoiskaPidarasow = new Scanner(System.in);
+            for (int i = 0; i < arr.length; i++) {
+
+                System.out.println("Введите гейство: ");
+                int value = scannerPoiskaPidarasow.nextInt();
+                arr[i] = value;
+            }
+            int min =arr[0];
+            for (int i=0;i< arr.length;i++) {
+            if(arr[i]<min)
+                min=arr[i];
+
+
+            }
+             System.out.println(min+" - ваше минимальное гейство"); */
+//char [][] pizdec=new char[2][2];
+//pizdec[0][0]='t';
+//  System.out.println(pizdec[0][0]);
+
+
+//numbers.add(index,element) meniaet mestami dannye iz indexa i propisanye v elemente
+//       ArrayList<Integer> numbers = new ArrayList<>();
+//      numbers.add(5);
+//          numbers.add(69);
+//numbers.add(1, 50);
+
+//  System.out.println(numbers.size());
+//  System.out.println(numbers.get(1));
+//numbers.remove(1);
+// udalaet blyat vse           numbers.clear();
+//      for(Integer el : numbers) {
+//        System.out.println(el);
+//linked list =Array but linkedlist sleplen v kuchu esli ty ne budesh meniat nichuja togda stav arive dla optimizciji
+// LinkedList<Float> numbers= new LinkedList<>();
+//numbers.add(5.6f);
+//    System.out.println(numbers);
+// v jave est' tolko ebanye metody,funkcyy - metody
+
+
+//import javax.sound.sampled.Line;
+
+//public class Main{
+//public static void main(String[]  args) {
+//    PiskaBobra("Idi");
+//    PiskaBobra("Nachuj suka");
+//    PiskaBobra("");
+//
+//    short num= 7;
+//int result1= sumy((short) 5,  num);
+//
+//short num2=9;
+//int result2= sumy((short) 4, num2 );
+//
+//int x= result1+result2;
+//    System.out.println(x);
+//
+//}
+//public static void PiskaBobra(String chuj){
+//    System.out.print(chuj);
+//    System.out.println("!");
+//}
+//
+//public static int sumy(short a, short b) {
+//    int res=a+b;
+//    String result ="wynik gejozy "+res;
+//    PiskaBobra(result);
+//    return res;
+//}
+
+
+
+//  }
+
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//        byte [] nums1= new byte[] {1,2,3};
+//int sum1 = summaArray(nums1);
+//        System.out.println("wynik pierwszy: " + sum1);
+// }
+//public static int summaArray(byte[] arr) {
+//    int summa = 0;
+//    for (byte i = 0; i < arr.length;i++)
+//        summa+=arr[i];
+//    return summa;
+//}
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//}
